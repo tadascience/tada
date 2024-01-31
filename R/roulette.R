4c819cda8f60af0de8719ac9e0e55c45ab56d39c
Romain Francois <romain@purrple.cat>
2024-01-31 07:53:37 +0100

    pkg_roulette(n=)

--- R/roulette.R
+++ R/roulette.R
@@ -1,17 +1,24 @@
-#' CRAN package name roulette
+#' package roulette
 #'
-#' Runs a roulette of available packages on CRAN
+#' Runs a packages roulette
 #'
 #' @param time How long in seconds the roulette runs. It will increasingly speed up.
-#' @param n number of packages in the roulette, packages are selected with [available.packages()]
+#' @param n number of packages in the roulette, packages are selected with [utils::available.packages()]. Ignored if `pkgs=` is given.
+#' @param pkgs Packages to show in the roulette.
 #' @param cols colors. The default is to pick `n` colors
 #'   from the built-in [grDevices::colors()].
 #'
 #' @return the last package, invisibly
 #'
 #' @examples
+#' pkg_roulette(
+#'   time = 2,
+#'   pkgs = sample(c("dplyr", "ggplot2", "data.table", "Rcpp", "arrow", "duckdb"))
+#' )
+#'
 #' \dontrun{
-#'   pkg_roulette(time = 3, n = 100)
+#'  # roulette of 100 available CRAN packages
+#'  pkg_roulette(time = 4, n = 100)
 #' }
 #'
 #' @importFrom grDevices colors
@@ -19,8 +26,12 @@
 #' @importFrom purrr pwalk
 #' @importFrom utils available.packages
 #' @export
-pkg_roulette <- function(time = 4, n = 100, cols = sample(colors(), n, replace = TRUE)) {
-  pkgs <- sample(available.packages()[, "Package"], n, replace = FALSE)
+pkg_roulette <- function(time = 4, n = 100, pkgs, cols = sample(colors(), n, replace = TRUE)) {
+  if (missing(pkgs)) {
+    pkgs <- sample(available.packages()[, "Package"], n, replace = FALSE)
+  } else {
+    n <- length(pkgs)
+  }
 
   x <- exp(seq(0, 3, length = n))
   times <- x / sum(x) * time
